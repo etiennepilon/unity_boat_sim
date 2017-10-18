d6949fdf3734d6a45a68dfcb5db39e102d329e11
Hello <anouk.hello.1@ens.etsmtl.ca>
2017-10-17 20:12:02 -0400

    Didactielle d'Oculus Touche et APIs

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Le code suivant sert a intercepter un ButtonDown event sur l’indexTrigger de
+        // l’oculus Touch et change la couleur de l’objet
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            Color actualColor = this.gameObject.GetComponent<Renderer>().material.color;
+            Color newColor = (actualColor == Color.gray ? Color.red : Color.grey);
+            this.gameObject.GetComponent<Renderer>().material.color = newColor;
+        }
+    }
+}
---
+++ Assets/OculusTouchInteractions.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OculusTouchInteractions : MonoBehaviour {
+    private GameObject grabbedObject;
+    private bool grabbing;
+    public OVRInput.Controller controller;
+    public string buttonName;
+    public float grabRadius;
+    public LayerMask grabMask;
+    void GrabObject()
+    {
+        grabbing = true;
+        RaycastHit[] hits;
+        hits = Physics.SphereCastAll(transform.position, grabRadius,
+        transform.forward, 0f, grabMask);
+        if (hits.Length > 0)
+        {
+            int closestHit = 0;
+            for (int i = 0; i < hits.Length; ++i)
+            {
+                if (hits[i].distance < hits[closestHit].distance) closestHit = i;
+            }
+            grabbedObject = hits[closestHit].transform.gameObject;
+            grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+            grabbedObject.transform.position = transform.position;
+            grabbedObject.transform.parent = transform;
+        }
+    }
+    void DropObject()
+    {
+        grabbing = false;
+        if (grabbedObject != null)
+        {
+            grabbedObject.transform.parent = null;
+            grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+            grabbedObject.GetComponent<Rigidbody>().velocity =
+            OVRInput.GetLocalControllerVelocity(controller);
+            grabbedObject.GetComponent<Rigidbody>().angularVelocity =
+            OVRInput.GetLocalControllerAngularVelocity(controller);
+            grabbedObject = null;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!grabbing && Input.GetAxis(buttonName) == 1) GrabObject();
+        if (grabbing && Input.GetAxis(buttonName) < 1) DropObject();
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+}

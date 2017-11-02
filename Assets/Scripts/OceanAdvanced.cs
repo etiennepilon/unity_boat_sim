bf3957d799b6573598d2567223be6c2e999536ec
Hello <anouk.hello.1@ens.etsmtl.ca>
2017-11-02 16:36:34 -0400

    test update

--- Assets/Scripts/OceanAdvanced.cs
+++ Assets/Scripts/OceanAdvanced.cs
@@ -36,21 +36,29 @@ public class OceanAdvanced : MonoBehaviour
   const int NB_INTERACTIONS = 64;
   static Wave[] waves =
   {
-    new Wave(5, 0f, 0f, 0f, new Vector2(1.0f,  0.2f)),
-    new Wave(5, 0f, 0f, 0f, new Vector2(1.0f,  3.0f)),
-    new Wave(5, 0f, 0f, 0f, new Vector2(2.0f,  4.0f)),
-    new Wave(5, 0f, 0f, 0f, new Vector2(-1.0f, 0.0f)),
-    new Wave(5, 0f, 0f, 0f,new Vector2(-1.0f, 1.2f))
-  };
-	/*
     new Wave(99, 1.0f, 1.4f, 0.9f, new Vector2(1.0f,  0.2f)),
     new Wave(60, 1.2f, 0.8f, 0.5f, new Vector2(1.0f,  3.0f)),
     new Wave(20, 3.5f, 0.4f, 0.8f, new Vector2(2.0f,  4.0f)),
     new Wave(30, 2.0f, 0.4f, 0.4f, new Vector2(-1.0f, 0.0f)),
     new Wave(10, 3.0f, 0.05f, 0.9f,new Vector2(-1.0f, 1.2f))
+	
+  };
+    /*
+    new Wave(99, 1.0f, 1.4f, 0.9f, new Vector2(1.0f,  0.2f)),
+    new Wave(60, 1.2f, 0.8f, 0.5f, new Vector2(1.0f,  3.0f)),
+    new Wave(20, 3.5f, 0.4f, 0.8f, new Vector2(2.0f,  4.0f)),
+    new Wave(30, 2.0f, 0.4f, 0.4f, new Vector2(-1.0f, 0.0f)),
+    new Wave(10, 3.0f, 0.05f, 0.9f,new Vector2(-1.0f, 1.2f))
+
+
+        new Wave(5, 0f, 0f, 0f, new Vector2(1.0f,  0.2f)),
+    new Wave(5, 0f, 0f, 0f, new Vector2(1.0f,  3.0f)),
+    new Wave(5, 0f, 0f, 0f, new Vector2(2.0f,  4.0f)),
+    new Wave(5, 0f, 0f, 0f, new Vector2(-1.0f, 0.0f)),
+    new Wave(5, 0f, 0f, 0f,new Vector2(-1.0f, 1.2f))
 	*/
 
-  void Awake()
+    void Awake()
   {
     Vector4[] v_waves = new Vector4[NB_WAVE];
     Vector4[] v_waves_dir = new Vector4[NB_WAVE];

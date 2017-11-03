d3bbf70e4dd3114e9ef822f0085578d1d2a426fa
Boudreault-Pilon <etienne.boudreault-pilon.1@ens.etsmtl.ca>
2017-11-02 22:04:34 -0400

    Added new scripts for controlling both with either Oculus One TOuch or Keyboard. Need to fine tune the control with one touch. Need to Change the Camera for OVR

---
+++ Assets/Scripts/KanoeController.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class KanoeController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private boatCoordinateManager boat;
+    private bool controllingWithOneTouch = false;
+    enum PaddlingState {NotPaddling, PaddlingLeft, PaddlingRight};
+    // --
+    // These values are to determine if the person is trying to circle
+    private double incrementalTurn = 0;
+    private bool goingLeft = false, goingRight = false;
+    // --
+    // All variables related to holding a Paddle
+    private bool holdingPaddle = true;
+    private double paddleAngleThreshold = 20.0 * 3.1416 / 180.0; // This is 10 degrees
+    // -- 
+    // All Variables related to the One Touch Controller
+    //public OVRInput.Controller controller;
+    private oneTouchCoordinates previousHandCoords, currentHandCoords;
+    public struct oneTouchCoordinates
+    {
+        public float zleft, zright;
+        public float dx, dy;
+        public oneTouchCoordinates(float z1, float z2, float dX, float dY)
+        {
+            zleft = z1;
+            zright = z2;
+            dx = dX;
+            dy = dY;
+        }
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        boat = new boatCoordinateManager(Time.fixedDeltaTime);
+        currentHandCoords = new oneTouchCoordinates();
+        previousHandCoords = new oneTouchCoordinates();
+    }
+    void FixedUpdate()
+    {
+        PaddlingState state = getPaddlingState();
+        double paddleDistance = 0.9;
+        if (state == PaddlingState.PaddlingLeft) boat.paddleLeftWithCorrectionFactor(paddleDistance, incrementalTurn);
+        else if (state == PaddlingState.PaddlingRight) boat.paddleRightWithCorrectionFactor(paddleDistance, incrementalTurn);
+        else boat.notPaddlingWithCorrection(incrementalTurn);
+
+        rb.MovePosition(new Vector3(boat.p_y, 0, boat.p_x));
+        Quaternion rotation = Quaternion.Euler(new Vector3(0, (float)(180.0 / 3.1416 * boat.heading), 0));
+        rb.MoveRotation(rotation);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+    // --
+    // This Method determines the side the user is Paddling 
+    private PaddlingState getPaddlingState()
+    {
+        PaddlingState state = PaddlingState.NotPaddling;
+        if (controllingWithOneTouch && holdingPaddle)
+        {
+            // -- TODO
+            state = getOneTouchPaddlingState();
+        }
+        else if (controllingWithOneTouch && !holdingPaddle)
+        {
+            // -- Should give a sign to the user to hold the paddle/close the hands
+            state = PaddlingState.NotPaddling;
+        }
+        else
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            if (moveHorizontal < 0) state = PaddlingState.PaddlingLeft;
+            else if (moveHorizontal > 0) state = PaddlingState.PaddlingRight;
+        }
+        updateTurningIncrement(state);
+        return state;
+    }
+    // --
+    // This function returns the deltaX = Xright - Xleft and deltaY = Yright - Yleft
+    //   in order to compute the angle. Its the equivalent as calculating the X, Y vector
+    //   between both hands. 
+    // It also returns zleft and zright. It will be required in order to know in which direction
+    //   the paddle is going. If Zleft_previous - Zleft_new < 0, we know we're moving forward.
+    // This assume local (relative to the user) coordinates.
+    private void updateHandPositionVector()
+    {
+        Vector3 leftHandCoords = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        Vector3 rightHandCoords = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        currentHandCoords.zleft = leftHandCoords.z; currentHandCoords.zright = rightHandCoords.z;
+        currentHandCoords.dx = rightHandCoords.x - leftHandCoords.x;
+        currentHandCoords.dy = rightHandCoords.y - leftHandCoords.y;
+    }
+    private PaddlingState getOneTouchPaddlingState()
+    {
+        PaddlingState state = PaddlingState.NotPaddling;
+        updateHandPositionVector();
+        if (currentHandCoords.dx == 0.0) return state;
+        // --
+        // Calculate the angle between the right and left hand using the right hand as reference
+        double theta = Math.Atan(currentHandCoords.dy / currentHandCoords.dx);
+        //Debug.Log(theta * 180.0/3.1416);
+        if (paddleIsInTheWater(theta) && paddleIsRightSide(theta))
+        {
+           if (paddleIsPushingWater(previousHandCoords.zright - currentHandCoords.zright))
+           {
+                state = PaddlingState.PaddlingRight;
+           }
+        }
+        else if (paddleIsInTheWater(theta) && paddleIsLeftSide(theta))
+        {
+           if (paddleIsPushingWater(previousHandCoords.zleft - currentHandCoords.zleft))
+           {
+                state = PaddlingState.PaddlingLeft;
+            }
+        }
+    
+        previousHandCoords = currentHandCoords;
+        return state;
+    }
+    private bool paddleIsInTheWater(double theta) { return Math.Abs(theta) > paddleAngleThreshold; }
+    private bool paddleIsPushingWater(double delta) { return delta > 0.003; }
+    private bool paddleIsRightSide(double theta) { return (theta < 0);}
+    private bool paddleIsLeftSide(double theta) { return (theta > 0); }
+
+    private void updateTurningIncrement(PaddlingState state)
+    {
+        if (state == PaddlingState.PaddlingLeft)
+        {
+            goingLeft = true;
+            if (goingRight)
+            {
+                incrementalTurn = 0;
+                goingRight = false;
+            }
+            incrementalTurn += Time.deltaTime;
+        }
+        else if (state == PaddlingState.PaddlingRight)
+        {
+            goingRight = true;
+            if (goingLeft)
+            {
+                incrementalTurn = 0;
+                goingLeft = false;
+            }
+            incrementalTurn += Time.deltaTime;
+        }
+    }
+}
--- Assets/Scripts/boatController.cs
+++ Assets/Scripts/boatController.cs
@@ -9,88 +9,155 @@
 using UnityEngine;
 using System;
 
-public class boatController{
-	// --
-	// Constant related to the geometry and forces of the boat
-	private const double c_mass = 100 /*kg*/, 
-	c_boat_width = 1.0 /*m*/,
-	c_boat_length = 3.0 /*m*/,
-	c_boat_height = 0.8 /*m*/,
-	c_percentage_of_immersion_in_water = 0.5,
-	c_front_drag_coeff = 0.3,
-	c_side_drag_coeff = 4,
-	c_water_mass_density = 1000 /* kg/m3 */;
-	public double c_forward_paddle_force = 120 /* N */;
+public class boatCoordinateManager {
+    // --
+    // Constant related to the geometry and forces of the boat
+    private const double c_mass = 100 /*kg*/,
+    c_boat_width = 1.0 /*m*/,
+    c_boat_length = 3.0 /*m*/,
+    c_boat_height = 0.8 /*m*/,
+    c_percentage_of_immersion_in_water = 0.5,
+    c_front_drag_coeff = 0.3,
+    c_side_drag_coeff = 1,
+    c_water_mass_density = 1000 /* kg/m3 */;
+    private double c_forward_paddle_force = 120 /* N */;
 
 
-	private double time_delta /* seconds */, c_boat_front_area, c_boat_side_area, inertial_moment, side_drag_distance; 
-	public double absolute_linear_speed { get; set;} /* m/s */
-	public double angular_speed { get; set;} /* rad/s */
-	public double heading {get; set;}
-	public float p_x {get; set;} 
-	public float p_y {get; set;}
-	// --
-	// Initialize with default values - possible to overload
-	public boatController(double delta_time)
-	{
-		absolute_linear_speed = 0;
-		angular_speed = 0;
-		time_delta = delta_time;
-		// -- Considered a perfect triangle for now.
-		c_boat_front_area = (c_percentage_of_immersion_in_water * c_boat_width) * (c_percentage_of_immersion_in_water * c_boat_height) / 2;
-		// -- Considered a rectangle for now
-		c_boat_side_area = (c_percentage_of_immersion_in_water * c_boat_length) * (c_boat_height);
-		inertial_moment = c_mass * (c_boat_length * c_boat_length) / 6;
-		side_drag_distance = c_boat_length / 2 * 0.9;
-		heading = 0; p_x = 0; p_y = 0;
-	}
-	/*
+    private double time_delta /* seconds */, c_boat_front_area, c_boat_side_area, inertial_moment, side_drag_distance;
+    public double absolute_linear_speed { get; set; } /* m/s */
+    public double angular_speed { get; set; } /* rad/s */
+    public double heading { get; set; }
+    public float p_x { get; set; }
+    public float p_y { get; set; }
+    // --
+    // Initialize with default values - possible to overload
+    public boatCoordinateManager(double delta_time)
+    {
+        absolute_linear_speed = 0;
+        angular_speed = 0;
+        time_delta = delta_time;
+        // -- Considered a perfect triangle for now.
+        c_boat_front_area = (c_percentage_of_immersion_in_water * c_boat_width) * (c_percentage_of_immersion_in_water * c_boat_height) / 2;
+        // -- Considered a rectangle for now
+        c_boat_side_area = (c_percentage_of_immersion_in_water * c_boat_length) * (c_boat_height);
+        inertial_moment = c_mass * (c_boat_length * c_boat_length) / 6;
+        side_drag_distance = c_boat_length / 2 * 0.9;
+        heading = 0; p_x = 0; p_y = 0;
+    }
+    /*
 	 This section calculates the drag of the water (rough estimate) and the linear speed.
 	*/
-	private double frontDrag()
-	{
-		return c_front_drag_coeff * c_water_mass_density * c_boat_front_area * (absolute_linear_speed * absolute_linear_speed)/2;
-	}
-	public void updateLinearVelocityWithForce(double external_force)
-	{
-		double linear_acc = (external_force - frontDrag ()) / c_mass;
-		absolute_linear_speed = absolute_linear_speed + linear_acc * time_delta;
-		if (absolute_linear_speed < 0) {
-			absolute_linear_speed = 0;
-		}
-	}
-	/*
+    private double frontDrag()
+    {
+        return c_front_drag_coeff * c_water_mass_density * c_boat_front_area * (absolute_linear_speed * absolute_linear_speed) / 2;
+    }
+    private void updateLinearVelocityWithForce(double external_force)
+    {
+        double linear_acc = (external_force - frontDrag()) / c_mass;
+        absolute_linear_speed = absolute_linear_speed + linear_acc * time_delta;
+        if (absolute_linear_speed < 0) {
+            absolute_linear_speed = 0;
+        }
+    }
+    /*
 	 This section calculates the drag of the water (rough estimate) and the angular speed.
 	*/
-	private double sideDrag()
-	{
-		// --
-		// Assumption: We can estimate the linear speed with the angular speed because for small time intervals, the delta is almost the same.
-		return c_side_drag_coeff * c_water_mass_density * c_boat_side_area * (angular_speed * angular_speed)/2;
-	}
-	// --
-	// Parameters
-	// - external_force: Force applied by the paddle
-	// - distance_from_center_of_mass: Distance of the paddle from the boat
-	// --
-	// Assumptions
-	// - The force is applied directly perpedicularly to the center of mass (the avatar is seated in the middle of the boat)
-	// - The drag on the boat is applied at the constant distance from the center of mass
-	// - Paddling to the left is a negative distance from the center of mass (thus, the drag becomes positive)
-	public void updateAngularVelocityWithForceAndDistance(double external_force, double distance_from_center_of_mass)
-	{
-		double angular_acc = 0;
-		if (angular_speed >= 0) {
-			angular_acc = (external_force * distance_from_center_of_mass - sideDrag () * side_drag_distance) / inertial_moment;
-		} else {
-			angular_acc = (external_force * distance_from_center_of_mass + sideDrag () * side_drag_distance) / inertial_moment;
-		}
-		angular_speed = angular_speed + angular_acc * time_delta;
-	}
+    private double sideDrag()
+    {
+        // --
+        // Assumption: We can estimate the linear speed with the angular speed because for small time intervals, the delta is almost the same.
+        double drag = c_side_drag_coeff * c_water_mass_density * c_boat_side_area * (angular_speed * angular_speed) / 2;
+        Debug.Log("Drag:" + drag);
+        return drag;
+    }
+    // --
+    // Parameters
+    // - external_force: Force applied by the paddle
+    // - distance_from_center_of_mass: Distance of the paddle from the boat
+    // --
+    // Assumptions
+    // - The force is applied directly perpedicularly to the center of mass (the avatar is seated in the middle of the boat)
+    // - The drag on the boat is applied at the constant distance from the center of mass
+    // - Paddling to the left is a negative distance from the center of mass (thus, the drag becomes positive)
+    private void updateAngularVelocityWithForceAndDistance(double external_force, double distance_from_center_of_mass)
+    {
+        double angular_acc = 0;
+        if (angular_speed >= 0) {
+            angular_acc = (external_force * distance_from_center_of_mass - sideDrag() * side_drag_distance) / inertial_moment;
+        } else {
+            angular_acc = (external_force * distance_from_center_of_mass + sideDrag() * side_drag_distance) / inertial_moment;
+        }
+        angular_speed = angular_speed + angular_acc * time_delta;
+    }
 
-	public void updateHeadingAndPosition(){
-		heading += angular_speed * time_delta;
-		p_x += (float)((absolute_linear_speed * time_delta)* Math.Cos (heading));
-		p_y += (float)((absolute_linear_speed * time_delta) * Math.Sin (heading));
-	}
+    private void updateAngularVelocityWithForceAndCorrection(double external_force, double distance_from_center_of_mass, double factor)
+    {
+        double angular_acc = 0;
+        if (angular_speed >= 0)
+        {
+            angular_acc = (applyFactor(external_force, factor) * distance_from_center_of_mass - sideDrag() * side_drag_distance) / inertial_moment;
+        }
+        else
+        {
+            angular_acc = (applyFactor(external_force, factor) * distance_from_center_of_mass + sideDrag() * side_drag_distance) / inertial_moment;
+        }
+        angular_speed = angular_speed + angular_acc * time_delta;
+        Debug.Log("Ang speed:" + angular_speed);
+    }
+    // --
+    // Every Update, the angle is 
+    private void updateHeadingAndPosition() {
+        heading += angular_speed * time_delta;
+        p_x += (float)((absolute_linear_speed * time_delta) * Math.Cos(heading));
+        p_y += (float)((absolute_linear_speed * time_delta) * Math.Sin(heading));
+    }
+    public void paddleLeftWithCorrectionFactor(double paddleDistanceFromBoatCenter, double factor)
+    {
+        updateAngularVelocityWithForceAndCorrection(c_forward_paddle_force, -paddleDistanceFromBoatCenter, factor);
+        updateLinearVelocityWithForce(c_forward_paddle_force);
+        updateHeadingAndPosition();
+    }
+
+    public void paddleRightWithCorrectionFactor(double paddleDistanceFromBoatCenter, double factor)
+    {
+        updateAngularVelocityWithForceAndCorrection(c_forward_paddle_force, paddleDistanceFromBoatCenter, factor);
+        updateLinearVelocityWithForce(c_forward_paddle_force);
+        updateHeadingAndPosition();
+    }
+    public void notPaddlingWithCorrection(double factor)
+    {
+        updateAngularVelocityWithForceAndCorrection(0, 0, factor);
+        updateLinearVelocityWithForce(0);
+        updateHeadingAndPosition();
+    }
+    // -- Receives Absolute distance of the paddle from the boat
+    public void paddleLeftWithPaddleDistance(double paddleDistanceFromBoatCenter)
+    {
+        updateAngularVelocityWithForceAndDistance(c_forward_paddle_force, -paddleDistanceFromBoatCenter);
+        updateLinearVelocityWithForce(c_forward_paddle_force);
+        updateHeadingAndPosition();
+    }
+    // -- Receives Absolute distance of the paddle from the boat
+    public void paddleRightWithPaddleDistance(double paddleDistanceFromBoatCenter)
+    {
+        updateAngularVelocityWithForceAndDistance(c_forward_paddle_force, paddleDistanceFromBoatCenter);
+        updateLinearVelocityWithForce(c_forward_paddle_force);
+        updateHeadingAndPosition();
+    }
+    // -- Receives Absolute distance of the paddle from the boat
+    public void notPaddling()
+    {
+        updateAngularVelocityWithForceAndDistance(0, 0);
+        updateLinearVelocityWithForce(0);
+        updateHeadingAndPosition();
+    }
+    // --
+    // This is a hack to avoid that the boat changes direction too much on first stride
+    // #SorryMom m
+    private double applyFactor(double x, double factor)
+    {
+        double den = 1 / Math.Exp(3 - factor);
+        if (den > 4) return x * 4;
+        else return x * den;
+    }
 }
